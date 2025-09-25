0603d9f109fb309fd13add1fa2743fcdad285737
magomezl <98543790+magomezl@users.noreply.github.com>
2025-09-25 19:54:11 +0200

    25-09-2025

---
+++ eclipse-workspace/Ficheros_1/src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,97 @@
+package ejercicio5;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import paquete_1.Utilidades;
+
+public class Ejercicio5 {
+	private final static String DATOSFILEOUT = "FicheroSerializacion.bin";
+	private static ObjectOutputStream oOS;
+	private static Scanner sn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		inicializar();
+//		escribirObjeto(obtenerDatos());
+		escribirObjeto(new Persona(new StringBuilder("Alfonso"), new StringBuilder("Gómez"), 
+				new StringBuilder("Borrego"), LocalDate.of(2020, 12, 21)));
+		System.out.println("Elemento contenidos en el fichero " + leerObjetos());
+	}
+	
+	
+
+	private static int leerObjetos() {
+		int contador = 0;
+		try (ObjectInputStream oIS = new ObjectInputStream(new FileInputStream
+					(new File(Utilidades.RUTA+DATOSFILEOUT)))){
+			
+			while(true) {
+				System.out.println((Persona) oIS.readObject());
+				++contador;
+			}
+		}catch (EOFException e) {
+			System.out.println("Fin de fichero");
+		}catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}	
+		return contador;
+	}
+
+
+
+	private static Persona obtenerDatos() {
+		Persona persona = new Persona();
+		System.out.println("DATOS DE USUARIO:");
+		System.out.println("\tNombre:");
+		persona.setNombre(new StringBuilder(sn.nextLine()));
+		System.out.println("\tPrimer Apellido:");
+		persona.setApellido1(new StringBuilder(sn.nextLine()));
+		System.out.println("\tSegundo Apellido:");
+		persona.setApellido2(new StringBuilder(sn.nextLine()));
+		return persona;
+	}
+
+
+
+	private static void escribirObjeto(Persona persona) {
+		try {
+			oOS.writeObject(persona);
+		} catch (IOException e) {
+			System.out.println("Error de escritura");
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void inicializar() {
+		try {
+			File file = new File(Utilidades.RUTA+DATOSFILEOUT);
+			 if (file.exists() && file.length()>0) {
+//				oOS = new MyObjectOutputStream(new FileOutputStream(file, true));
+				oOS = new ObjectOutputStream(new FileOutputStream(file, true)) {
+					@Override
+					protected void writeStreamHeader() throws IOException {
+						}
+				};
+				 
+			}else {
+				oOS = new ObjectOutputStream(new FileOutputStream(file));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encuentra el fichero");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error de entrada/salida");
+			e.printStackTrace();
+		}
+	}
+
+}
--- eclipse-workspace/Ficheros_1/src/ejercicio5/Persona.java
+++ eclipse-workspace/Ficheros_1/src/ejercicio5/Persona.java
@@ -5,44 +5,52 @@ import java.time.LocalDate;
 
 public class Persona implements Serializable{
 	
-	private static final long serialVersionUID = -2099173592037393849L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7087622712910013542L;
 	
-	private String nombre;
-	private String apellido1;
-	private String apellido2;
+	
+	private StringBuilder nombre;
+	private StringBuilder apellido1;
+	private StringBuilder apellido2;
+	
+	
+	
+	
 	private LocalDate nacimiento;
 	 
 	public Persona() {
 	}
 
-	public Persona(String nombre, String apellido1, String apellido2, LocalDate nacimiento) {
+	public Persona(StringBuilder nombre, StringBuilder apellido1, StringBuilder apellido2, LocalDate nacimiento) {
 		this.nombre = nombre;
 		this.apellido1 = apellido1;
 		this.apellido2 = apellido2;
 		this.nacimiento = nacimiento;
 	}
 
-	public String getNombre() {
+	public StringBuilder getNombre() {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
+	public void setNombre(StringBuilder nombre) {
 		this.nombre = nombre;
 	}
 
-	public String getApellido1() {
+	public StringBuilder getApellido1() {
 		return apellido1;
 	}
 
-	public void setApellido1(String apellido1) {
+	public void setApellido1(StringBuilder apellido1) {
 		this.apellido1 = apellido1;
 	}
 
-	public String getApellido2() {
+	public StringBuilder getApellido2() {
 		return apellido2;
 	}
 
-	public void setApellido2(String apellido2) {
+	public void setApellido2(StringBuilder apellido2) {
 		this.apellido2 = apellido2;
 	}
 
@@ -53,4 +61,14 @@ public class Persona implements Serializable{
 	public void setNacimiento(LocalDate nacimiento) {
 		this.nacimiento = nacimiento;
 	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", nacimiento="
+				+ nacimiento + "]";
+	}
+
+	
+	
+	
 }
